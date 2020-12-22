fe1bae929e3e734642bafc2178b6cc1d4b65f663
ch8zer <ch8zer@protonmail.com>
2020-12-21 20:43:56 -0500

    Added ppustatus

--- src/nes/ppu/registers.rs
+++ src/nes/ppu/registers.rs
@@ -24,12 +24,20 @@ pub struct Registers {
 
 impl Registers {
     const PPUCTRL_ADDR: usize = 0x2000;
+    const PPUMASK_ADDR: usize = 0x2001;
+    const PPUSTATUS_ADDR: usize = 0x2002;
 
     pub fn new() -> Self {
         let mut ppuctrl = BiasedBitSet::default();
         ppuctrl.bias(6, 0); // Grounded on a NES
         let ppumask = BiasedBitSet::default();
         let ppustatus = BiasedBitSet::default();
+        for i in 0..6 {
+            // Since these values are don't cares,
+            // zero them out so that reads
+            // are easy to implement
+            ppuctrl.bias(i, 0);
+        }
         let oamdata = BiasedBitSet::default();
         let oamaddr = BiasedBitSet::default();
         let ppuscroll = BiasedBitSet::default();
@@ -60,12 +68,21 @@ impl Registers {
     pub fn set_vblank(&mut self, is_enabled: bool) {
         self.ppuctrl.set(7, is_enabled as u8)
     }
+
+    // TODO effective ppu reg read methods
 }
 
 impl MemoryMapped for Registers {
     fn load(&mut self, addr: usize) -> IronNesResult<u8> {
         self.latch = match addr {
             Self::PPUCTRL_ADDR => self.ppuctrl.cast(),
+            Self::PPUMASK_ADDR => self.ppumask.cast(),
+            Self::PPUSTATUS_ADDR => {
+                let v = self.ppumask.cast() | (self.latch & 0x1f);
+                // Subsequent reads clear bit 7
+                self.ppustatus.set(7, 0);
+                v
+            }
             _ => {
                 return Err(IronNesError::MemoryError(format!(
                     "Address not addressable: {:04x}",
@@ -80,6 +97,10 @@ impl MemoryMapped for Registers {
         self.latch = data;
         match addr {
             Self::PPUCTRL_ADDR => Ok(self.ppuctrl.store(data)),
+            Self::PPUMASK_ADDR => Ok(self.ppumask.store(data)),
+            Self::PPUSTATUS_ADDR => {
+                Err(IronNesError::MemoryError(format!("PPUSTATUS is readonly")))
+            }
             _ => Err(IronNesError::MemoryError(format!(
                 "Address not addressable: {:04x}",
                 addr
@@ -97,6 +118,20 @@ mod tests {
         let mut r = Registers::new();
         r.set_vblank(true);
         assert_eq!(0x80, r.load(Registers::PPUCTRL_ADDR)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_bus_ppustatus() -> IronNesResult<()> {
+        let mut r = Registers::new();
+        r.latch = 0b0101_1010u8;
+        r.ppustatus.store(0xf0);
+        assert_eq!(0b1111_1010u8, r.load(Registers::PPUSTATUS_ADDR)?);
+        assert_eq!(
+            0b0111_1010u8,
+            r.load(Registers::PPUSTATUS_ADDR)?,
+            "Subsequent reads should clear bit 7"
+        );
         Ok(())
     }
 }
